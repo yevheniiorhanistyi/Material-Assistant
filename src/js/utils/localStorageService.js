6820d33890e4403167354ff415208c2561294415
Yevhenii Orhanistyi <organistiy@gmail.com>
2024-03-22 19:22:19 +0100

    feat: add services for get/save/remove data from localStorage

---
+++ src/js/utils/localStorageService.js
@@ -0,0 +1,9 @@
+export const saveToLocalStorage = (key, value) => {
+  localStorage.setItem(key, value);
+};
+
+export const getFromLocalStorage = (key) => localStorage.getItem(key);
+
+export const removeFromLocalStorage = (key) => {
+  localStorage.removeItem(key);
+};
