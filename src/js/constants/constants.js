75272892b76bd1056d8eed8448c1a4ff0938e2e3
Yevhenii Orhanistyi <organistiy@gmail.com>
2024-04-02 20:52:02 +0200

    feat: add constants with language and min/max portion size

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -1,5 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
+export const MIN_PORTION_SIZE = 270;
+export const MAX_PORTION_SIZE = 450;
 export const MAX_PORTION_TABS = 12;
 
 export const INITIAL_TABS = [
@@ -9,3 +11,19 @@ export const INITIAL_TABS = [
   { uid: uuidv4(), tabText: 'Arkusz 4', selected: false },
   { uid: uuidv4(), tabText: 'Arkusz 5', selected: false },
 ];
+
+export const LANG_PL = {
+  required: 'To pole jest wymagane',
+  email: 'To pole wymaga prawidłowego adresu e-mail',
+  number: 'To pole wymaga liczby',
+  integer: 'To pole wymaga wartości całkowitej',
+  url: 'To pole wymaga prawidłowego adresu URL strony internetowej',
+  tel: 'To pole wymaga prawidłowego numeru telefonu',
+  maxlength: `Długość tego pola musi być < ${1}`,
+  minlength: `Długość tego pola musi być > ${1}`,
+  min: `Minimalna wartość dla tego pola to ${1}`,
+  max: `Maksymalna wartość dla tego pola to ${1}`,
+  pattern: 'Proszę dopasować żądany format',
+  equals: 'Dwa pola nie są takie same',
+  default: 'Proszę wprowadzić poprawną wartość',
+};
