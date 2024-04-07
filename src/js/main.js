63bc9151df0e2d87020b5d29f395886b09f51b8a
Yevhenii Orhanistyi <organistiy@gmail.com>
2024-04-07 12:43:20 +0200

    feat: add initModal function

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
-import handleTabLogic from './modules/tabs/tabs.js';
 import initHeader from './modules/header/initHeader.js';
 import initPortionTabs from './modules/tabs/initPortionTabs.js';
+import initModal from './modules/modal/initModal.js';
+import handleTabLogic from './modules/tabs/tabs.js';
 import generatePortionForm from './modules/portionForm/generatePortionForm.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   initHeader();
   initPortionTabs();
+  initModal();
   handleTabLogic('portionTabs', 'portionFormData', generatePortionForm);
   generatePortionForm();
 });
