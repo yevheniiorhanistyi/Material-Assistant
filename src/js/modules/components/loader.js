3cdc622c57d4a4e08b7b301be97d0720d82914cd
Yevhenii Orhanistyi <organistiy@gmail.com>
2024-04-17 14:38:00 +0200

    feat: add loader component

---
+++ src/js/modules/components/loader.js
@@ -0,0 +1,13 @@
+const loader = () => `
+  <div class="loader-container">
+    <div class="loader">
+        <span class="circle-wrapper" role="progressbar">
+            <svg viewBox="22 22 44 44">
+              <circle class="circle" cx="44" cy="44" r="20.2" fill="none" stroke-width="3.6"></circle>
+            </svg>
+        </span>
+    </div>
+  </div>
+  `;
+
+export default loader;
