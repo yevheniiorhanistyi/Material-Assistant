dd970e4d670298d200b5a06c31518e2eebe845ca
Yevhenii Orhanistyi <organistiy@gmail.com>
2024-04-17 14:44:08 +0200

    feat: add initCommentsSection, initSnackbarContainer on main file

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import initHeader from './modules/header/initHeader.js';
 import initPortionTabs from './modules/tabs/initPortionTabs.js';
 import initModal from './modules/modal/initModal.js';
+import initSnackbarContainer from './modules/snackbar/initSnackbarContainer.js';
+import initCommentsSection from './modules/comments/initCommentsSection.js';
 import handleTabLogic from './modules/tabs/tabs.js';
 import generatePortionForm from './modules/portionForm/generatePortionForm.js';
 
@@ -8,6 +10,8 @@ window.addEventListener('DOMContentLoaded', () => {
   initHeader();
   initPortionTabs();
   initModal();
+  initCommentsSection();
+  initSnackbarContainer();
   handleTabLogic('portionTabs', 'portionFormData', generatePortionForm);
   generatePortionForm();
 });
