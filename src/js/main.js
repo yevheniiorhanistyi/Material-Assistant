90c61f991353aa63a629472e863e91938c56af34
Yevhenii Orhanistyi <organistiy@gmail.com>
2024-05-03 10:11:38 +0200

    feat: import drawer in main file

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,23 @@
 import initHeader from './modules/header/initHeader.js';
 import initMain from './modules/main/initMain.js';
+import initDrawer from './modules/drawer/initDrawer.js';
 import initPortionTabs from './modules/tabs/initPortionTabs.js';
-import initNotifications from './modules/notifications/initNotifications.js';
 import initModal from './modules/modal/initModal.js';
 import initSnackbarContainer from './modules/snackbar/initSnackbarContainer.js';
 import initComments from './modules/comments/initComments.js';
 import handleTabLogic from './modules/tabs/tabs.js';
-import handleNotifications from './modules/notifications/handleNotifications.js';
 import generatePortionForm from './modules/portionForm/generatePortionForm.js';
+import handleDrawer from './modules/drawer/handleDrawer.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   initHeader();
   initPortionTabs();
   initMain();
-  initNotifications();
+  initDrawer();
   initModal();
   initSnackbarContainer();
   initComments();
-  handleNotifications();
   handleTabLogic('portionTabs', 'portionFormData', generatePortionForm);
+  handleDrawer();
   generatePortionForm();
 });
