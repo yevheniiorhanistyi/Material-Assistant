5c8b909f119d52607fe2806c1cac4f79c9068bae
Yevhenii Orhanistyi <organistiy@gmail.com>
2024-05-25 11:29:25 +0200

    feat: add functionality for convert kg to percent

---
+++ src/js/modules/forms/portionForm/convertToPercent.js
@@ -0,0 +1,39 @@
+import { createElement } from '../../../utils/index.js';
+
+const convertToPercent = () => {
+  const mainEl = document.querySelector('.main');
+  const convert = document.querySelector('.convert');
+  const formControl = convert.querySelector('.form-control');
+  const materialInput = document.querySelector('#material-input');
+  const mixerSizeSelect = document.querySelector('#mixer-size-select');
+  const overlay = document.querySelector('.convert__overlay') || createElement('div', 'convert__overlay');
+
+  const onChangeValue = () => {
+    const value = (Number(formControl.value) * 100) / Number(mixerSizeSelect.value);
+    materialInput.value = Number(value.toFixed(1));
+  };
+
+  const closeConvertElement = () => {
+    convert.classList.remove('show');
+    formControl.removeEventListener('input', onChangeValue);
+    formControl.removeEventListener('keydown', onEnterPress);
+    overlay.removeEventListener('click', closeConvertElement);
+    overlay.remove();
+  };
+
+  const onEnterPress = (e) => {
+    if (e.key === 'Enter') closeConvertElement();
+  };
+
+  const openConvertElement = () => {
+    convert.classList.add('show');
+    formControl.addEventListener('input', onChangeValue);
+    formControl.addEventListener('keydown', onEnterPress);
+    overlay.addEventListener('click', closeConvertElement);
+    mainEl.append(overlay);
+  };
+
+  openConvertElement();
+};
+
+export default convertToPercent;
